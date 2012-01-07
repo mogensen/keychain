2ff163b277523e809e75eb72430ba9e19060289d
Frederik Mogensen <sodiaf@gmail.com>
2012-01-07 19:19:59 +0100

    Initial script to get an account and password from the osx keychain

---
+++ keychain.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+GETPASS=0
+GETUSER=0
+
+while [ "$1" != "" ]
+do
+	case $1
+		in
+		-p) GETPASS=1;
+			shift 1;;
+		-u) GETUSER=1;
+			shift 1;;
+		-a) ACCOUNT=$2;
+			shift 2;;
+		-v) verbose=1;
+			shift 1;;
+		*)  echo "Option [$1] not one of  [p, u, a, v]";    # Error (!)
+			exit;;
+	esac
+done
+
+SEC=`security find-generic-password -a $ACCOUNT -g 2>&1`
+
+function getAttribute() {
+	echo `echo "$SEC" | grep "$1" | cut -d \" -f 4`
+}
+
+# Get username (account)
+if test $GETUSER -gt 0 ; then
+	getAttribute "acct"
+	exit 0
+fi
+
+# Get password
+if test $GETPASS -gt 0 ; then
+	echo `echo "$SEC" | grep "password" | cut -d \" -f 2`
+	exit 0
+fi
